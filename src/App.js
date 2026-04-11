93d0134e4bec194887becc9ab4c80162bab3b76f
CuevaGermane <lic.cuevagerman@gmail.com>
2026-04-11 16:00:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Bienvenidos al Sistema de la Escuela Sagrada Familia</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
