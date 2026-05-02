8c467df9653ab353793677d3b2daf3ed1caac79d
CuevaGermane <lic.cuevagerman@gmail.com>
2026-05-02 10:25:14 -0300

    tabs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
+import logo from './logoEscuelaTransparente.png';
 import "./App.css";
 import Login from "./login"; // archivo login.js ✔
 import Home from "./home";
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import ItemListContainerPersona from './components/ItemListContainerPersonas';
+import './App.css';
 
 function App() {
   return (
@@ -17,39 +19,8 @@ function App() {
           <Route path="/Home" element={<Home />} />
         </Routes>
       </div>
-import logo from './logoEscuelaTransparente.png';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemListContainerPersona from './components/ItemListContainerPersonas';
-import './App.css';
 
-function App() {
-  return (
-        <BrowserRouter> 
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bienvenidos al Sistema de la Escuela Sagrada Familia</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-
-      <main> 
-        <Routes>
-         <Route exact path="/" element={<ItemListContainerPersona/>}/>  
-         <Route exact path="/personas" element={<ItemListContainerPersona/>}/>  
-      </Routes>
-     </main>
-   </BrowserRouter>
-
-
-  );
+ );
 }
 
 export default App;
